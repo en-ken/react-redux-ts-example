31410d018684cf801f624603ef61e9acea1f19aa
en-ken <inonakano.kawaz.taikai.never.knows@gmail.com>
2018-12-07 08:11:46 +0900

    chore: 1st commit. configure the base environments.

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: path.join(__dirname, './src/index.tsx'),
+  },
+  output: {
+    path: path.join(__dirname, '/dist/scripts'),
+    publicPath: '/scripts',
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'tslint-loader',
+            options: {
+              typeCheck: true,
+              fix: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'awesome-typescript-loader',
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  devServer: {
+    contentBase: path.join(__dirname, './dist'),
+    watchContentBase: true,
+  }
+};
